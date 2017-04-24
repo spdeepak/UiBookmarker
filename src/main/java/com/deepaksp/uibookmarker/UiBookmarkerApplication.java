0a67dbb9536d6364b6ee2bb29a496ab149735ff9
Deepak <speedpak1991@gmail.com>
2017-04-24 12:59:33 +0100

    First Commit with Spring Boot nature

---
+++ src/main/java/com/deepaksp/uibookmarker/UiBookmarkerApplication.java
@@ -0,0 +1,12 @@
+package com.deepaksp.uibookmarker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UiBookmarkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UiBookmarkerApplication.class, args);
+	}
+}
---
+++ src/test/java/com/deepaksp/uibookmarker/UiBookmarkerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.deepaksp.uibookmarker;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UiBookmarkerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
